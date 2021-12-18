f6fcdd9bc28aeff30e7dc84e93a1a6d74ed3fb50
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2021-12-18 20:18:47 +0100

    Full conf automation

--- build_conf/build_conf.sh
+++ build_conf/build_conf.sh
@@ -4,5 +4,6 @@ CUR_DIR=$(pwd -P)
 # This two step process is performed to separate the CI logic (autosourcing the config)
 # from the configuration build step (this script)
 TARGET_DIR="$CUR_DIR"/../src/dodekaserver/resources/conf
-rm -rf "$TARGET_DIR" ||
-mv "$CUR_DIR"/autosourced_config "$TARGET_DIR"
+echo "Building conf..."
+rm -rf "$TARGET_DIR" || exit 1
+cp "$CUR_DIR"/autosourced_config "$TARGET_DIR" || exit 1

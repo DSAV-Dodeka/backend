771a067af11b7a652995cd598cd83cfb5c148808
tiptenbrink <75669206+tiptenbrink@users.noreply.github.com>
2022-01-02 20:52:54 +0100

    New test data dump

--- localtest/pydest.py
+++ localtest/pydest.py
@@ -2,5 +2,8 @@ from pydantic import BaseModel
 import datetime
 from datetime import timezone
 
-x = datetime.datetime.now(timezone.utc).timestamp()
-print(x)
+# x = datetime.datetime.now(timezone.utc).timestamp()
+# print(x)
+i = "d764baa92d6724624740b140a7b592668e011680cdb1d47f12befeb17183c53e2b6516cac766c2b82a504fda37751fd6c188d2ce646420d6bf196bcfbb9bb646"
+z = bytes.fromhex(i)
+print(z.decode("utf-8"))

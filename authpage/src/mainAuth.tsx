e7434ed79bfd38b974c155866dc9d5af102ff652
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-09-14 17:23:07 +0200

    Fix register loaded in dev

--- authpage/src/mainAuth.tsx
+++ authpage/src/mainAuth.tsx
@@ -2,11 +2,10 @@ import './index.css'
 import React from 'react'
 import {createRoot} from "react-dom/client";
 import Auth from './Auth'
-import Register from "./Register";
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(<React.StrictMode>
-    <Register />
+    <Auth />
 </React.StrictMode>)
 

22b373460d0f63392ccda1e8ed08af85d234c365
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-09-30 19:10:34 +0200

    Fix vite vars

--- authpage/src/config.ts
+++ authpage/src/config.ts
@@ -1,6 +1,6 @@
 export default
 {
-    "auth_location": import.meta.env.AUTHPAGE_AUTH_URL,
-    "client_location": import.meta.env.AUTHPAGE_CLIENT_URL,
+    "auth_location": import.meta.env.VITE_AUTHPAGE_AUTH_URL,
+    "client_location": import.meta.env.VITE_AUTHPAGE_CLIENT_URL,
     "allowed_redirects": ["account/email/"]
 }

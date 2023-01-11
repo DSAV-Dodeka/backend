a868554e872be3158deae3e5ba08c5e4d39f8d9c
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-01-11 13:56:22 +0100

    Fix temp points.json

--- src/apiserver/data/file.py
+++ src/apiserver/data/file.py
@@ -6,7 +6,14 @@ from apiserver import res_path
 async def load_json(filename: str) -> dict:
     pth = res_path.joinpath(filename + ".json")
     if not pth.exists():
-        fakedata = {"leander": 69}
+        fakedata = {
+            "points": [
+                {"Naam": "Arnold", "Punten": 12},
+                {"Naam": "Arnold", "Punten": 12},
+                {"Naam": "Arnold", "Punten": 12},
+            ]
+        }
+
         with open(pth, "x") as f:
             json.dump(fakedata, f)
 

d9b20d80e7e4988260eaa166753778fb475df55f
tiptenbrink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-03 09:32:47 +0100

    remove gunicorn.sh as no longer used

--- gunicorn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -a
-export APISERVER_CONFIG=./localenv.toml
-export GUNICORN_CMD_ARGS="--bind=0.0.0.0:4241 --workers=20"
-poetry run gunicorn apiserver.app:app -w 20 -k uvicorn.workers.UvicornWorker

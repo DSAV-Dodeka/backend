64379b100d78e34d084efccdac8453dcd66d13c9
sdrent <S.Drent@student.tudelft.nl>
2023-12-07 18:14:30 +0100

    made requested changes regarding id formatting

--- src/apiserver/data/api/trainings.py
+++ src/apiserver/data/api/trainings.py
@@ -13,19 +13,13 @@ async def add_training_event(
     description: str = "",
 ) -> list[str]:
     """breaks up a training into their subcategories and insert them into the class_events table.
-    event_id will be created in the form of: "training[dd/mm/yyyy][category]"
+    event_id will be created in the form of: "training[yyyy/dd/mm][category]"
     we make the assumption that there are no two trainings in a day"""
     idList = []
     event_rows: list[LiteralDict] = []
     for subCategory in categories:
-        subEventId = usp_hex(f'training{event_date.isoformat()}{subCategory}')
+        subEventId = f'training{event_date.isoformat()}{subCategory}'
 
-        if len(subEventId) > MAX_EVENT_ID_LEN:
-            raise DataError(
-                f"event_id is longer than {MAX_EVENT_ID_LEN}! Please use a short,"
-                " descriptive name, like 'nsk_weg_2023'.",
-                "event_id_too_long",
-            )
         idList.append(subEventId)
 
         event_row: LiteralDict = {
